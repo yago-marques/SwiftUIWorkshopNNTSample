84d095ffad75573c78c55346f8fa191cb9abe701
Yago Marques <84297648+yago-marques@users.noreply.github.com>
2023-08-18 13:49:43 -0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Yago Marques on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
